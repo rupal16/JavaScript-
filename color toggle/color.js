4d4524d8c99fd818668e1a325e032bb7b549f158
Rupal Jain <rupaljain.1699@gmail.com>
2018-09-09 11:42:50 +0530

    Add files via upload

---
+++ color toggle/color.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("button");
+var ispurple = false;
+
+
+
+button.addEventListener("click",function(){
+	if (ispurple) {
+		document.body.style.background="white";
+		ispurple= false;
+	}
+
+	else {
+		document.body.style.background="purple";
+		ispurple= true;
+	}
+
+});
