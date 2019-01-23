2e47f56df81f03169866d11fbdfc2e70e8f4dbf9
Rupal Jain <rupaljain.1699@gmail.com>
2019-01-23 08:29:43 +0530

    Create script.js

---
+++ Color Changer/script.js
@@ -0,0 +1,18 @@
+var button = document.querySelector("button");
+
+
+button.addEventListener("click", function(){
+
+	/*if white
+	 	change to purple
+	 	else
+	 		change to white*/
+  
+   	
+   	document.body.classList.toggle("purple");
+   	
+    
+
+   
+});
+	
